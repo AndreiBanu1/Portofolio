e94b215070b1b87ac62280c940a6c7fae2e83982
ElBanu <andreibanu97@yahoo.com>
2022-10-11 13:54:25 +0300

    header and hero

--- components/About.tsx
+++ components/About.tsx
@@ -10,7 +10,7 @@ export default class About extends Component<Props, State> {
 
   render() {
     return (
-      <div className="flex flex-col relative h-screen text-center md-text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+      <div className="flex flex-col relative h-screen text-center amd-text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
         <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
           About
         </h3>
