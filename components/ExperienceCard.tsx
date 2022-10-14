9118d12de50f94e8f08a2e081575016f9d9d36fb
ElBanu <andreibanu97@yahoo.com>
2022-10-14 17:17:25 +0300

    backend added

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -15,7 +15,7 @@ export default function ExperienceCard({}: Props) {
   return (
     <article
       className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px]
-     snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-y-auto"
+     snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-y-auto scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80"
     >
       <motion.img
         initial={{
